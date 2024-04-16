d66316b76ac46e260e26604f8ac1c600a197310e
Ubludor <denis.r.maslov@gmail.com>
2024-04-16 21:48:57 +0300

    Добавил счтетчик кадров в секунду

--- main.py
+++ main.py
@@ -15,19 +15,19 @@ def getFrame(i):
 	frame = base64.b64encode(buffer)
 	frame = str(frame)[2:-1]
 	return "data:image/jpeg;base64,"+frame
-def sendFrames(i):
+def sendFrames(i,fps,current_time):
 	
-	current_time = datetime.datetime.now().time()
+	
 	t = str(current_time.hour)+":"+str(current_time.minute)+":"+str(current_time.second)
 	frameBase64 = getFrame(i)
 	r = {}
-	if i%50==0:
-		r = {"Время":t,"b64":frameBase64,"Кадр":i,"Название файла":"Очень толстый кот сидит на лавочке.mp4"}
+	if True: #i%50==0:
+		r = {"Время":t,"b64":frameBase64,"Номер кадра":i,"Кадров в секунду": fps,"Название файла":"Очень толстый кот сидит на лавочке.mp4"}
 	else:
-		r = {"Время":t,"Кадр":i,"Название файла":"Очень толстый кот сидит на лавочке.mp4"}
+		r = {"Время":t,"Кадр":i,"Кадров в секунду": fps,"Название файла":"Очень толстый кот сидит на лавочке.mp4"}
 	jsonn = json.dumps(r) # note i gave it a different name
 	
-	print(t)
+	print(i)
 	return jsonn
 		
 
@@ -35,11 +35,21 @@ async def send_message():
     uri = "ws://localhost:8080/ws?type=1" 
     
     async with websockets.connect(uri) as websocket:
+        second = 0
+        frames = 0
+        fps = 0
         i=0
         while True:
             i=i+1
-            message = sendFrames(i)
+            current_time = datetime.datetime.now().time()
+            if second == current_time.second:
+                frames = frames+1
+            else:
+                fps = frames
+                frames = 0
+                second = current_time.second
+            message = sendFrames(i,fps,current_time)
             await websocket.send(message)
-            await asyncio.sleep(1/60)  # Ожидание 1 секунды перед отправкой следующего сообщения
+            await asyncio.sleep(1/250)  # Ожидание 1 секунды перед отправкой следующего сообщения
 
 asyncio.get_event_loop().run_until_complete(send_message())
